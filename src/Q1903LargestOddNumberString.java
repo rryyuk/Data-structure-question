e59999c287cbef6e47242d7403513655bc84f798
rryyuk <agranshul15@gmail.com>
2022-11-21 21:53:59 +0530

    largest odd number string

--- src/Q1903LargestOddNumberString.java
+++ src/Q1903LargestOddNumberString.java
@@ -2,8 +2,8 @@ public class Q1903LargestOddNumberString {
     public String largestOddNumber(String num) {
         for(int i = num.length()-1; i >= 0; i--) {
             char currentNumber = num.charAt(i);
-            if(currentNumber%2 == 1) {
-                return num.substring(0, i+1);
+            if (currentNumber % 2 == 1) {
+                return num.substring(0, i + 1);
             }
         }
         return "";
