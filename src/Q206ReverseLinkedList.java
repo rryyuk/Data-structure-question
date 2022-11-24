81557b2fadf7399984792b1cfc1ce4f9e2b01c72
rryyuk <agranshul15@gmail.com>
2022-11-24 22:18:32 +0530

    Reverse a linked list

---
+++ src/Q206ReverseLinkedList.java
@@ -0,0 +1,14 @@
+import q2addtwonumbers.ListNode;
+
+public class Q206ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
